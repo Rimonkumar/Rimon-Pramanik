2b0188dc15138633010cebbeef550ff1d6317f28
Rimonkumar <22-46957-1@student.aiub.edu>
2025-08-29 22:57:55 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import sitemap from "vite-plugin-sitemap"; // ⚡ default import
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://rimonpramanik.netlify.app", // তোমার লাইভ সাইটের URL
+      routes: [
+        "/",          // Home page
+        "/about",     // About page
+        "/projects",  // Projects page
+        "/contact",   // Contact page
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
